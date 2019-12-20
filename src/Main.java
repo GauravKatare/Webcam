93e196e4706abbda8c918287f1e8eca8e9c7ad2c
GauravKatare <kataregaurav1501@gmail.com>
2019-12-20 23:56:42 +0530

    Video is saving

--- src/Main.java
+++ src/Main.java
@@ -1,50 +1,71 @@
-import com.github.sarxos.webcam.*;
+import com.github.sarxos.webcam.Webcam;
+import com.github.sarxos.webcam.WebcamResolution;
+import com.xuggle.mediatool.IMediaWriter;
+import com.xuggle.mediatool.ToolFactory;
+import com.xuggle.xuggler.ICodec;
+import com.xuggle.xuggler.IPixelFormat;
+import com.xuggle.xuggler.IVideoPicture;
+import com.xuggle.xuggler.video.ConverterFactory;
+import com.xuggle.xuggler.video.IConverter;
 import javafx.application.Application;
-import javafx.embed.swing.SwingNode;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.layout.AnchorPane;
-import javafx.scene.layout.StackPane;
 import javafx.stage.Stage;
 
-import javax.swing.*;
 import java.awt.*;
+import java.awt.image.BufferedImage;
+import java.io.File;
 import java.io.IOException;
-
 
 public class Main extends Application {
 
-    static WebcamPanel webcamPanel;
     @Override
-    public void start(Stage stage) {
-        final SwingNode swingNode = new SwingNode();
-        createAndSetSwingContent(swingNode);
-        StackPane pane = new StackPane();
-        pane.getChildren().add(swingNode);
-        stage.setScene(new Scene(pane, 640, 480));
-        stage.show();
+    public void start(Stage primaryStage) throws Exception {
     }
 
-    private void createAndSetSwingContent(final SwingNode swingNode) {
-        SwingUtilities.invokeLater(new Runnable() {
-            @Override
-            public void run() {
-                swingNode.setContent(webcamPanel);
-            }
-        });
+
+    public static void main(String[] args) throws InterruptedException {
+        Main videoWriter = new Main();
+        videoWriter.startVideoRecording();
     }
 
-    public static void main(String[] args) {
-        System.out.println("Controller is running");
-        Webcam webcam= Webcam.getDefault();
-        //webcam.setViewSize(new Dimension(640,480));
-        webcam.setViewSize(WebcamResolution.VGA.getSize());// better way to do above tas
-        webcamPanel=new WebcamPanel(webcam,false);
-        webcamPanel.setImageSizeDisplayed(true);
-        webcamPanel.setFPSDisplayed(true);
-        webcamPanel.setMirrored(true);
-        webcamPanel.start();
-        launch(args);
+    private void startVideoRecording() throws InterruptedException {
+        File saveFile = new File("saved.mp4");
+
+        //Initialize media writer
+        IMediaWriter writer = ToolFactory.makeWriter(saveFile.getName());
+        //Set video recording size
+        Dimension size = WebcamResolution.VGA.getSize();
+
+        writer.addVideoStream(0, 0, ICodec.ID.CODEC_ID_H264, size.width, size.height);
+
+        long start = System.currentTimeMillis();
+        Webcam webcam = openWebcam(size);
+
+        for (int i = 0; i < 50; i++) {
+            BufferedImage image = ConverterFactory.convertToType(webcam.getImage(), BufferedImage.TYPE_3BYTE_BGR);
+            IConverter converter = ConverterFactory.createConverter(image, IPixelFormat.Type.YUV420P);
+
+            IVideoPicture frame = converter.toPicture(image, (System.currentTimeMillis() - start) * 1000);
+            frame.setKeyFrame(i == 0);
+            frame.setQuality(100);
+
+            writer.encodeVideo(0, frame);
+
+            Thread.sleep(20);
+        }
+
+        writer.close();
+        webcam.close();
+        System.out.println("Video recorded to the file: " + saveFile.getAbsolutePath());
+    }
+
+    private Webcam openWebcam(Dimension size) {
+        Webcam webcam = Webcam.getDefault();
+        webcam.setViewSize(size);
+        webcam.open();
+        return webcam;
     }
 }
