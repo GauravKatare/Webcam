6f5c6bc5fa9f7dbfc259cbb88d1f71ce155ece07
GauravKatare <kataregaurav1501@gmail.com>
2019-12-21 03:44:40 +0530

    Video running on webcampanel

--- src/Controller.java
+++ src/Controller.java
@@ -2,65 +2,51 @@ import com.github.sarxos.webcam.*;
 import javafx.embed.swing.SwingNode;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
-import javafx.scene.image.ImageView;
 import javafx.scene.layout.AnchorPane;
-import javax.imageio.ImageIO;
-import java.awt.*;
-import java.io.File;
+import javax.swing.*;
 
 import java.io.IOException;
 
 public class Controller {
     @FXML
-    ImageView imagehold;
-    @FXML
     Button captureButton;
     @FXML
-    AnchorPane anchorPane;
-    @FXML
-    WebcamPanel webcamPanel;
-    @FXML
+    AnchorPane anchorpane;
+
     Webcam webcam;
-    //VideoPlayerThread vpt;
+    WebcamPanel webcamPanel;
+    SwingNode swingNode;
+    private boolean isRunning=false;
 
     public Controller()throws IOException
-    {
-        System.out.println("Controller is running");
-        webcam=Webcam.getDefault();
-        anchorPane=new AnchorPane();
-//        webcam.setViewSize(new Dimension(320,240));
-        webcam.setViewSize(WebcamResolution.VGA.getSize());// better way to do above tas
-        webcamPanel=new WebcamPanel(webcam,false);
-        webcamPanel.setImageSizeDisplayed(true);
-        webcamPanel.setFPSDisplayed(true);
-        webcamPanel.setMirrored(true);
-        webcam.addWebcamListener(new WebcamListener() {
-            @Override
-            public void webcamOpen(WebcamEvent webcamEvent) {
-                System.out.println("Open Cam");
-            }
+    { }
 
+    public void createAndSetSwingContent(final SwingNode swingNode) {
+        SwingUtilities.invokeLater(new Runnable() {
             @Override
-            public void webcamClosed(WebcamEvent webcamEvent) {
-                System.out.println("Close Cam");
-            }
-
-            @Override
-            public void webcamDisposed(WebcamEvent webcamEvent) {
-                System.out.println("Dispose Cam");
-            }
-
-            @Override
-            public void webcamImageObtained(WebcamEvent webcamEvent) {
-                //System.out.println("Image Taken");
+            public void run() {
+                swingNode.setContent(webcamPanel);
             }
         });
-        webcam.open();
-
     }
 
     public void capture()
     {
-        //webcampaneltry.startvideo();
+        if(!isRunning)
+        {
+            isRunning=true;
+            System.out.println("Video Start");
+            webcamPanel=new WebcamPanel(webcam);
+            webcamPanel.setFPSDisplayed(true);
+            webcamPanel.setMirrored(true);
+            createAndSetSwingContent(swingNode);
+            anchorpane.getChildren().add(swingNode);
+        }
+        else
+        {
+            isRunning = false;
+            webcamPanel.stop();
+            System.out.println("Video Paused");
+        }
     }
 }
--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,4 @@
-import com.github.sarxos.webcam.Webcam;
-import com.github.sarxos.webcam.WebcamResolution;
+import com.github.sarxos.webcam.*;
 import com.xuggle.mediatool.IMediaWriter;
 import com.xuggle.mediatool.ToolFactory;
 import com.xuggle.xuggler.ICodec;
@@ -8,64 +7,65 @@ import com.xuggle.xuggler.IVideoPicture;
 import com.xuggle.xuggler.video.ConverterFactory;
 import com.xuggle.xuggler.video.IConverter;
 import javafx.application.Application;
+import javafx.embed.swing.SwingNode;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.layout.AnchorPane;
 import javafx.stage.Stage;
 
+import javax.swing.*;
 import java.awt.*;
 import java.awt.image.BufferedImage;
 import java.io.File;
 import java.io.IOException;
 
-public class Main extends Application {
-
+public class Main extends Application
+{
+    static Webcam webcam;
     @Override
-    public void start(Stage primaryStage) throws Exception {
+    public void start(Stage primaryStage) throws Exception{
+        FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("WebcamWindow.fxml"));
+        Parent pane = fxmlLoader.load();
+        Controller controller = fxmlLoader.getController();
+        controller.webcam=getwebcam();
+        final SwingNode swingNode = new SwingNode();
+        controller.swingNode=swingNode;
+        primaryStage.setTitle("Hello World");
+        primaryStage.setScene(new Scene(pane, 800, 800));
+        primaryStage.show();
+        //webcampanel that support video feed
     }
 
-
-    public static void main(String[] args) throws InterruptedException {
-        Main videoWriter = new Main();
-        videoWriter.startVideoRecording();
+    public static void main(String[] args) throws IOException {
+        launch(args);
     }
 
-    private void startVideoRecording() throws InterruptedException {
-        File saveFile = new File("saved.mp4");
+    private Webcam getwebcam()
+    {
+        webcam= Webcam.getDefault();
+        webcam.setViewSize(WebcamResolution.VGA.getSize());// better way to do above task
+        webcam.addWebcamListener(new WebcamListener() {
+            @Override
+            public void webcamOpen(WebcamEvent webcamEvent) {
+                System.out.println("Open Cam");
+            }
 
-        //Initialize media writer
-        IMediaWriter writer = ToolFactory.makeWriter(saveFile.getName());
-        //Set video recording size
-        Dimension size = WebcamResolution.VGA.getSize();
+            @Override
+            public void webcamClosed(WebcamEvent webcamEvent) {
+                System.out.println("Close Cam");
+            }
 
-        writer.addVideoStream(0, 0, ICodec.ID.CODEC_ID_H264, size.width, size.height);
+            @Override
+            public void webcamDisposed(WebcamEvent webcamEvent) {
+                System.out.println("Dispose Cam");
+            }
 
-        long start = System.currentTimeMillis();
-        Webcam webcam = openWebcam(size);
-
-        for (int i = 0; i < 50; i++) {
-            BufferedImage image = ConverterFactory.convertToType(webcam.getImage(), BufferedImage.TYPE_3BYTE_BGR);
-            IConverter converter = ConverterFactory.createConverter(image, IPixelFormat.Type.YUV420P);
-
-            IVideoPicture frame = converter.toPicture(image, (System.currentTimeMillis() - start) * 1000);
-            frame.setKeyFrame(i == 0);
-            frame.setQuality(100);
-
-            writer.encodeVideo(0, frame);
-
-            Thread.sleep(20);
-        }
-
-        writer.close();
-        webcam.close();
-        System.out.println("Video recorded to the file: " + saveFile.getAbsolutePath());
-    }
-
-    private Webcam openWebcam(Dimension size) {
-        Webcam webcam = Webcam.getDefault();
-        webcam.setViewSize(size);
-        webcam.open();
-        return webcam;
+            @Override
+            public void webcamImageObtained(WebcamEvent webcamEvent) {
+                System.out.println("Image Taken");
+            }
+        });
+        return  webcam;
     }
 }
