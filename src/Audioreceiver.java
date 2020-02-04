5f44884b702376f4777f3048944159e9803b27b6
GauravKatare <kataregaurav1501@gamil.com>
2020-02-04 10:54:35 +0530

    Issue with Audio

--- src/Audioreceiver.java
+++ src/Audioreceiver.java
@@ -1,3 +1,5 @@
+import javax.sound.sampled.*;
+import java.io.ByteArrayInputStream;
 import java.io.IOException;
 import java.io.ObjectInputStream;
 
@@ -21,7 +23,25 @@ class Audioreceiver implements Runnable
             if (obj instanceof Myaudio)
             {
                 System.out.println("Audio is working");
+                Myaudio myaudio=(Myaudio)obj;
+                byte[] data=myaudio.getAudioData();
+                ByteArrayInputStream bis = new ByteArrayInputStream(data);
+                System.out.println(myaudio.getTimestamp());
 
+
+                AudioFormat format = new AudioFormat(8000.0f, 16, 1, true, true);
+                DataLine.Info dataLineInfo = new DataLine.Info(SourceDataLine.class, format);
+                SourceDataLine speakers = null;
+                try {
+                    speakers = (SourceDataLine) AudioSystem.getLine(dataLineInfo);
+                    speakers.open(format);
+                    speakers.start();
+                }
+                catch (LineUnavailableException e) {
+                    e.printStackTrace();
+                }
+
+                speakers.write(data,0,data.length);
             }
             else {
                 System.out.println("Error in Audio");
--- src/Myaudio.java
+++ src/Myaudio.java
@@ -7,6 +7,22 @@ public class Myaudio implements Serializable
     private long timestamp;
     private byte[] AudioData;
 
+    public long getTimestamp() {
+        return timestamp;
+    }
+
+    public void setTimestamp(long timestamp) {
+        this.timestamp = timestamp;
+    }
+
+    public byte[] getAudioData() {
+        return AudioData;
+    }
+
+    public void setAudioData(byte[] audioData) {
+        AudioData = audioData;
+    }
+
     public Myaudio(byte[] AudioData, long timestamp)
     {
         this.AudioData= AudioData;
